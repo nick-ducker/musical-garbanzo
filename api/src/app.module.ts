fbf0d5550d35c19eb04a6c3fa57c9ffb82d8a17b
Nick Ducker <nick_ducker@live.com>
2022-03-30 21:01:33 +1100

    add station module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherApiModule } from './weather-api/weather-api.module';
+import { StationModule } from './station/station.module';
 
 @Module({
-  imports: [WeatherApiModule],
+  imports: [WeatherApiModule, StationModule],
   controllers: [AppController],
   providers: [AppService],
 })
